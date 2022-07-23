7953c2a42a7620f7dc8d63716fdfb8b024f9e7b4
leegeonwoo <dlrjsdnzzzz@naver.com>
2022-07-23 13:52:27 +0900

    shoppingmall update and delete

--- src/com/gw/dao/ProductDAO.java
+++ src/com/gw/dao/ProductDAO.java
@@ -48,4 +48,93 @@ public class ProductDAO {
 		}
 		return list;
 	}// selectAllProducts() { 
-} // ProductDAO {
+	
+	public void insertProduct(ProductVO pVo) {
+		String sql = "insert into product values(product_seq.nextval, ?, ?, ?, ?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pVo.getName());
+			pstmt.setInt(2, pVo.getPrice());
+			pstmt.setString(3, pVo.getPictureUrl());
+			pstmt.setString(4, pVo.getDescription());
+			pstmt.executeUpdate(); // 실행
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
+	
+	// c Read u d
+	public ProductVO selectProductByCode(String code) {
+		String sql = "select * from product where code=?";
+		ProductVO pVo = null;
+		try {
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				conn = DBManager.getConnection();
+				pstmt = conn.prepareStatement(sql);
+				pstmt.setString(1, code);
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					pVo = new ProductVO();
+					pVo.setCode(rs.getInt("code"));
+					pVo.setName(rs.getString("name"));
+					pVo.setPrice(rs.getInt("price"));
+					pVo.setPictureUrl(rs.getString("pictureUrl"));
+					pVo.setDescription(rs.getString("description"));
+				}
+			}catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				DBManager.close(conn, pstmt, rs);
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return pVo;
+	}
+	
+	// c r Uptade d
+	
+	public void updateProduct(ProductVO pVo) {
+		String sql = "update product set name=?, price=?, pictureurl=?, description=? where code=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pVo.getName());
+			pstmt.setInt(2, pVo.getPrice());
+			pstmt.setString(3, pVo.getPictureUrl());
+			pstmt.setString(4, pVo.getDescription());
+			pstmt.setInt(5, pVo.getCode());
+			pstmt.executeUpdate();// 쿼리문 실행한다.
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
+	
+	public void deleteProduct(String code) {
+		String sql = "delete product where code=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, code);
+			pstmt.executeUpdate();// 쿼리문 실행
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, pstmt);
+		}
+	}
+} 
